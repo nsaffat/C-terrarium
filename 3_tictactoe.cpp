220da7056fc2e4ab0e8069767e3bee24f7f6369e
nsaffat <60826643+nsaffat@users.noreply.github.com>
2020-10-12 02:34:43 -0700

    Update and rename 3-tictactoe to 3_tictactoe.cpp

--- 3_tictactoe.cpp
+++ 3_tictactoe.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+ 
 using namespace std;
 
+//declaring and initializing variables
 char ttt[10] = {'0','1','2','3','4','5','6','7','8','9'};
-int turn=1, i;
+char input;
+int turn, i, Xwins=0, Owins=0, again=1;
 
 
+
+//function to reprint board outline with placeholders
 void board() {
   cout << endl << endl;
   cout << " "<< ttt[1]<<" | "<< ttt[2]<<" | "<<ttt[3]<<" " << endl;
@@ -15,16 +20,20 @@ void board() {
   cout << endl;
 }
 
+//function for 1 round of input/gameplay
 void gameplay () {
-  char input;
-  
+
+  //gameplay() is the same for X or O's turn. Odd numbered turn means X, even numbered turn means O
   if (turn % 2 == 1)  {
     cout << "Player X turn. ";}
   else {
     cout << "Player O turn. ";}
-  cout << "Enter location (1-9) of mark " ;
+
+  //prompt to enter your next move
+  cout << "Enter location of move " ;
   cin >> input;
 
+  //for loop to check if player chose a valid location for their mark
   for (i=0; i<10; i++) {
     if (input == ttt[i]) {
       if (turn % 2 == 1)  {
@@ -40,6 +49,7 @@ void gameplay () {
   }
 }
 
+//function to check if three in a row is present of the board
 bool threeinarow() {
   if (ttt[1] == ttt[2] && ttt[2] == ttt[3]) {
     return true;}
@@ -66,17 +76,27 @@ bool threeinarow() {
 
 
 int main() {
-  board ();
 
+while (again==1) {
+  
+  board ();
+  turn = 1;
+
+  //when three in a row happens, while loop breaks. turn = 10 means that no winner declared but board is filled up so must be a tie
   while (turn <= 10) {
     gameplay ();
     board ();
     threeinarow ();
     if (threeinarow() == true) {
       if (turn % 2 == 1)  {
-	cout << "winner is Player O \n" << endl; }
+	cout << "winner is Player O \n";
+	Owins = Owins + 1;
+      }
       else {
-	cout << "winner is Player X \n" << endl; }
+	cout << "winner is Player X \n";
+	Xwins = Xwins + 1;
+      }
+      cout << " Player X has " << Xwins << " wins & Player O has " << Owins << " wins \n ";
       break;}
       
     if (turn == 10) {
@@ -84,12 +104,26 @@ int main() {
       break; }
   }
   
+  //prompt to restart game
+  cout << endl <<  "Play again? type 0 for no, 1 for yes ";
+  cin >> again;
+
+  //if user wants to play again, the board is reset
+  if (again==1){
+    ttt[1]= '1';
+    ttt[2]= '2';
+    ttt[3]= '3';
+    ttt[4]= '4';
+    ttt[5]= '5';
+    ttt[6]= '6';
+    ttt[7]= '7';
+    ttt[8]= '8';
+    ttt[9]= '9';
+  }
+ }
   return 0;
+
 }
-
-
-
-
 
 
 
